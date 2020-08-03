899c1622d5b600fb7cf86fd7211d2a6f34a6e567
unknown <hzh_innovation@163.com>
2020-08-03 19:32:48 +0800

    [hzh] feat: add only have strike and 10th not strike

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -6,12 +6,20 @@ public class BowlingGame {
     public int countSource(int[] numberOfDown) {
         int source = 0;
 
-        for (int i = 0; i < ROUND * 2; i += 2) {
-            if (numberOfDown[i] + numberOfDown[i + 1] >= 10) {
-                source += (numberOfDown[i + 2] + 10);
-            }else {
-                source += ((numberOfDown[i] + numberOfDown[i + 1]));
+        for (int i = 0; i < ROUND * 2 - 1;) {
+            if (numberOfDown[i] == 10) {
+                source += (numberOfDown[i + 1] + numberOfDown[i + 2] + 10);
+                i++;
             }
+            else {
+                if (numberOfDown[i] + numberOfDown[i + 1] >= 10) {
+                    source += (numberOfDown[i + 2] + 10);
+                } else {
+                    source += ((numberOfDown[i] + numberOfDown[i + 1]));
+                }
+                i+=2;
+            }
+
         }
 
         return source;
