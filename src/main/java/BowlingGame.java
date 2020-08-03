c41ee34d089e038bbefc06aff9c536d66f6c0262
unknown <hzh_innovation@163.com>
2020-08-03 18:09:57 +0800

    [hzh] feat: add only have spare but 10th no strike

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -2,7 +2,14 @@ import java.util.Arrays;
 
 public class BowlingGame {
     public int countSource(int[] numberOfDown) {
-        int source = Arrays.stream(numberOfDown).sum();
+        int source = 0;
+
+        for (int i = 0; i < numberOfDown.length; i += 2) {
+            source += (numberOfDown[i] + numberOfDown[i + 1]);
+            if (numberOfDown[i] + numberOfDown[i + 1] >= 10) {
+                source += numberOfDown[i + 2];
+            }
+        }
 
         return source;
     }
