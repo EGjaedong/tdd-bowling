63cdbf15d736f4c1adca799a5e57005e8e8908f8
unknown <hzh_innovation@163.com>
2020-08-04 12:14:12 +0800

    [hzh] feat: fixed test input

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -28,7 +28,7 @@ public class BowlingGameTest {
     @Test
     void should_right_when_have_spare_but_no_strike_and_10th_no_strike_and_no_spare() {
         BowlingGame bowlingGame = new BowlingGame();
-        int[] numberOfDown = {1, 2, 5, 6, 1, 2, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 2, 1};
+        int[] numberOfDown = {1, 2, 5, 5, 1, 2, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 2, 1};
 
         int result = bowlingGame.countSource(numberOfDown);
         int expect = 51;
@@ -38,7 +38,7 @@ public class BowlingGameTest {
     @Test
     void should_right_when_have_spare_and_10th_have_spare_but_no_strike() {
         BowlingGame bowlingGame = new BowlingGame();
-        int[] numberOfDown = {1, 2, 5, 5, 1, 2, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 7, 7, 1};
+        int[] numberOfDown = {1, 2, 5, 5, 1, 2, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 7, 3, 1};
 
         int result = bowlingGame.countSource(numberOfDown);
         int expect = 59;
@@ -68,17 +68,27 @@ public class BowlingGameTest {
     @Test
     void should_right_when_have_both_strike_and_spare_but_not_10th() {
         BowlingGame bowlingGame = new BowlingGame();
-        int[] numberOfDown = {1, 2, 10, 6, 9, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 3, 4};
+        int[] numberOfDown = {1, 2, 10, 6, 4, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 3, 4};
 
         int result = bowlingGame.countSource(numberOfDown);
-        int expect = 79;
+        int expect = 74;
         assertEquals(expect, result);
     }
 
     @Test
     void should_right_when_all_condition_in_middle_and_spare_at_10th() {
         BowlingGame bowlingGame = new BowlingGame();
-        int[] numberOfDown = {1, 2, 10, 6, 9, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 7, 4, 5};
+        int[] numberOfDown = {1, 2, 10, 6, 4, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 7, 3, 5};
+
+        int result = bowlingGame.countSource(numberOfDown);
+        int expect = 82;
+        assertEquals(expect, result);
+    }
+
+    @Test
+    void should_right_when_all_condition_in_middle_and_strike_at_10th() {
+        BowlingGame bowlingGame = new BowlingGame();
+        int[] numberOfDown = {1, 2, 10, 6, 4, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 10, 5, 5};
 
         int result = bowlingGame.countSource(numberOfDown);
         int expect = 87;
@@ -86,12 +96,12 @@ public class BowlingGameTest {
     }
 
     @Test
-    void should_right_when_all_condition_in_middle_and_strike_at_10th() {
+    void should_right_when_all_strike() {
         BowlingGame bowlingGame = new BowlingGame();
-        int[] numberOfDown = {1, 2, 10, 6, 9, 3, 2, 4, 3, 2, 3, 1, 3, 2, 1, 3, 4, 10, 5, 8};
+        int[] numberOfDown = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
 
         int result = bowlingGame.countSource(numberOfDown);
-        int expect = 95;
+        int expect = 300;
         assertEquals(expect, result);
     }
 }
